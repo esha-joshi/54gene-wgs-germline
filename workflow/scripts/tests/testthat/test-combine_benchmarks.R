fc5a4a3b7ead5349291e5b24141f5028b7d9ea2d
Esha Joshi <esha.joshi@54gene.com>
2022-10-14 12:36:09 -0400

    test: add test for create.dataset

--- workflow/scripts/tests/testthat/test-combine_benchmarks.R
+++ workflow/scripts/tests/testthat/test-combine_benchmarks.R
@@ -1,6 +1,6 @@
 ## This test file is designed to be run from workflow/scripts
 ## with the command `testthat::test_file("tests/testthat/test-combine_benchmarks.R", reporter = default_reporter())`
-
+library(hms)
 source("../../combine_benchmarks.R")
 
 col.types <- cols(
@@ -18,19 +18,37 @@ col.types <- cols(
 
 col.names <- c("s","h:m:s","max_rss","max_vms","max_uss","max_pss","io_in","io_out","mean_load",	"cpu_time", "process", "rule")
 
+# test input files
 test.file1 <- "testthat_resources/benchmarks/21small.tsv"
 test.file2 <- "testthat_resources/benchmarks/21small_1_of_3.tsv"
 
-test_that("read.benchmarks properly reads in tsv file", {
-    #create expected out df
-    vals <- c(5.6752, 5, 113.45, 2586, 107,	108, 80.51, 0.00,	72.63,	0.2, "21small", "benchmarks")
-    tempdf <- data.frame(matrix(nrow=1, data=vals))
-    tbl <- as_tibble(tempdf)
-    tbl$X2 = hms::hms(seconds = 5, minutes = 0)
-    exp.df <- tbl %>% mutate_at(vars(-c("X2", "X11", "X12")), as.double)
-    colnames(exp.df) <- col.names
 
+# test df for test.file1
+vals <- c(5.6752, 5, 113.45, 2586, 107,	108, 80.51, 0.00,	72.63,	0.2, "21small", "benchmarks")
+tempdf <- data.frame(matrix(nrow=1, data=vals))
+tbl <- as_tibble(tempdf)
+tbl$X2 = hms::hms(seconds = 5, minutes = 0)
+test.file1.df <- tbl %>% mutate_at(vars(-c("X2", "X11", "X12")), as.double)
+colnames(test.file1.df) <- col.names
+
+test_that("read.benchmarks properly reads in tsv file", {
+    # create test output using input file
     test.out <- read.benchmarks(test.file1)
 
-    expect_identical(test.out, exp.df)
+    expect_identical(test.out, test.file1.df)
+})
+
+test_that("create.dataset combines benchmarking data correctly", {
+  # append a row for exp.df with data for test.file2
+  exp.df <- test.file1.df %>% add_row(
+  "s" = 88.5649, "h:m:s" = hms(seconds=28, minutes = 1),	"max_rss" = 410.99,
+           "max_vms" = 2588.63, "max_uss"=403.48, "max_pss"=404.22,
+           "io_in"=8.14, "io_out"=2.33, "mean_load"=75.60, "cpu_time"=67.23,
+           "process"="21small_1_of_3", "rule"="benchmarks")
+  # vector of input files
+  invec <- c(test.file1, test.file2)
+  # generate expected output
+  test.out <- create.dataset(invec)
+
+  expect_identical(test.out, exp.df)
 })
